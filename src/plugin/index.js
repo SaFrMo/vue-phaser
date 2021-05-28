d07a3b1c58afbd67da520a2f5b8dffbc4e3ec296
Sander Moolin <sandermoolin@gmail.com>
2021-05-28 11:32:40 -0400

    cleanup

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -1,19 +1,9 @@
 import components from './components'
 import gameMixin from './core/gameMixin'
 
-const output = (Vue, opts) => {
-    // check for store
-    // if (!store) {
-    //     throw new Error("Please provide vuex store as a `store` option.")
-    // }
-
-    // store.registerModule({ states, mutations, actions });
+const output = (Vue, opts = {}) => {
+    // register mixins
     Vue.mixin(gameMixin)
-
-    // // prep global vars
-    // Vue.game = null
-    // Vue.games = {}
-    // Vue.gamesCreated = 0
 
     // register components
     components.forEach((component) => {
