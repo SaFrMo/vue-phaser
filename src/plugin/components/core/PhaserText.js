070ac4dffd846879d5f2170bd928d1baebf9c19a
Sander Moolin <sandermoolin@gmail.com>
2021-06-01 16:16:06 -0400

    phaser-text

---
+++ src/plugin/components/core/PhaserText.js
@@ -0,0 +1,36 @@
+import base from '../common/base'
+import movable from '../../mixins/movable'
+import scalable from '../../mixins/scalable'
+
+export default {
+    ...base,
+    name: 'phaser-text',
+    mixins: [movable, scalable],
+    props: {
+        phaserStyle: {
+            type: Object,
+            default: () => ({})
+        },
+        text: {
+            default: ''
+        }
+    },
+    created() {
+        this.target = this.$scene.add.text(this.x, this.y, this.text.toString())
+        if (this.$host) {
+            this.$host.add(this.target)
+        }
+        this.refreshScale()
+    },
+    watch: {
+        text(newVal) {
+            this.target.setText(newVal)
+        }
+    },
+    beforeDestroy() {
+        if (this.target && this.target.destroy) {
+            this.target.destroy()
+        }
+    }
+}
+
--- src/plugin/components/index.js
+++ src/plugin/components/index.js
@@ -3,11 +3,13 @@ import PhaserGame from './core/PhaserGame'
 import PhaserScene from './core/PhaserScene'
 import PhaserShape from './core/PhaserShape'
 import PhaserSprite from './core/PhaserSprite'
+import PhaserText from './core/PhaserText'
 
 export default [
     PhaserContainer,
     PhaserGame,
     PhaserScene,
     PhaserShape,
-    PhaserSprite
+    PhaserSprite,
+    PhaserText
 ]
