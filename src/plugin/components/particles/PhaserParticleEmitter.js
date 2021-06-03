15c032272556f332c141f6e53e1352acadc96a45
Sander Moolin <sandermoolin@gmail.com>
2021-06-03 17:01:15 -0400

    particle fixes

--- src/plugin/components/particles/PhaserParticleEmitter.js
+++ src/plugin/components/particles/PhaserParticleEmitter.js
@@ -1,7 +1,4 @@
 import base from '../common/base'
-import gameObject from '../../mixins/gameObject'
-import movable from '../../mixins/movable'
-import scalable from '../../mixins/scalable'
 
 export default {
     ...base,
@@ -12,7 +9,6 @@ export default {
             default: () => ({})
         }
     },
-    // mixins: [gameObject, movable, scalable],
     created() {
         if (!this.$particles) {
             console.warn('no particle emitter')
@@ -20,9 +16,6 @@ export default {
         }
 
         this.target = this.$particles.createEmitter({
-            // ...this.movableProps,
-            // ...this.scalableProps,
-            // ...this.gameObjectProps,
             ...this.config
         })
     },
