8190398276c8bac7e3c955a61e0c36f094a32295
Sander Moolin <sandermoolin@gmail.com>
2021-06-09 16:44:57 -0400

    removing container destroys children

--- src/plugin/components/core/PhaserContainer.js
+++ src/plugin/components/core/PhaserContainer.js
@@ -5,8 +5,20 @@ export default {
     ...base,
     name: 'phaser-container',
     mixins: [customProps, depth, gameObject, movable, scalable],
+    props: {
+        keepChildren: {
+            type: Boolean,
+            default: false
+        }
+    },
     created() {
         this.target = this._host = this.$scene.add.container()
     },
+    beforeDestroy() {
+        const destroyChildren = !this.keepChildren
+        this.target.removeAll(destroyChildren)
+
+        this.target.destroy()
+    }
 }
 
