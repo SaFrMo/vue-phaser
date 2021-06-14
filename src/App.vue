faab7ab18b783f73836489befc77400200179994
Sander Moolin <sandermoolin@gmail.com>
2021-06-14 17:12:27 -0400

    wip on groups

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,21 @@
                 auto-start
                 :preload-queue="preloadQueue"
             >
-                <phaser-sprite sprite-key="space" x="400" y="300" />
+                <phaser-group ref="group" group-key="sprites">
+                    <phaser-sprite sprite-key="space" />
+                </phaser-group>
+
+                <phaser-group-child
+                    group-key="sprites"
+                    sprite-key="space"
+                    :x="item.x"
+                    :y="item.y"
+                    scale="0.2"
+                    :key="item.id"
+                    v-for="item in items"
+                />
+
+                <phaser-text :text="count" x="10" y="10" z="100" />
             </phaser-scene>
         </phaser-game>
     </div>
@@ -22,7 +36,34 @@ export default Vue.extend({
     data() {
         return {
             preloadQueue: ['space.png'],
+            items: [],
+            lowBound: 0,
+            y: 300,
+            count: 0,
         }
+    },
+    mounted() {
+        // this.update()
+        setInterval(() => {
+            this.items.push({
+                id: Date.now(),
+                y: Math.random() * 400,
+                x: Math.random() * 600,
+            })
+            if (this.items.length > 10) {
+                this.items.unshift()
+            }
+
+            // TODO: check why group isn't instantiating properly
+
+            this.count = this.$refs.group.target.countActive()
+        }, 300)
+    },
+    methods: {
+        update() {
+            this.y = Math.sin(Date.now() * 0.001) * 100 + 300
+            requestAnimationFrame(this.update)
+        },
     },
 })
 </script>
