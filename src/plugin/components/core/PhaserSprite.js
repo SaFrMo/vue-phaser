97d2d7b7aff56576761c597339a29ebb94abf033
Sander Moolin <sandermoolin@gmail.com>
2021-06-15 14:54:45 -0400

    group updates

--- src/plugin/components/core/PhaserSprite.js
+++ src/plugin/components/core/PhaserSprite.js
@@ -8,7 +8,6 @@ export default {
     props: {
         spriteKey: {
             type: String,
-            required: true
         },
         usePhysics: {
             type: Boolean,
--- src/plugin/components/groups/PhaserGroupChild.js
+++ src/plugin/components/groups/PhaserGroupChild.js
@@ -13,7 +13,6 @@ export default {
         },
         spriteKey: {
             type: String,
-            required: true
         },
     },
     computed: {
