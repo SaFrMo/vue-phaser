5c7bcdf1ea8e46e4a0217ca1c3ab19ac3472414d
Sander Moolin <sandermoolin@gmail.com>
2021-06-15 14:37:02 -0400

    Revert "Revert "group as object pool working""
    This reverts commit d4eaecd74a7a5312c5048d1869df490bf9273cb7.

--- src/plugin/components/core/PhaserSprite.js
+++ src/plugin/components/core/PhaserSprite.js
@@ -16,25 +16,22 @@ export default {
         }
     },
     created() {
-        let factory
         let addToHost = true
-        let args = []
+        let args = [this.x, this.y, this.spriteKey]
 
         switch (true) {
             case this.$host && this.$host.type === 'Group':
-                factory = this.$host.create.bind(this.$host)
                 addToHost = false
-                args = [null, null, false]
+                this.target = this.$host.create(...args, null, null, false)
                 break
             case this.usePhysics && this.$scene.physics:
-                factory = this.$scene.physics.add.sprite
+                this.target = this.$scene.physics.add.sprite(...args)
                 break
             default:
-                factory = this.$scene.add.sprite
+                this.target = this.$scene.add.sprite(...args)
                 break
         }
 
-        this.target = factory(this.x, this.y, this.spriteKey, ...args)
         if (this.$host && addToHost) {
             this.$host.add(this.target)
         }
