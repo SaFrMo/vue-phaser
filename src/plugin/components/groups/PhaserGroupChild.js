99c5435ad855ae132a45433710858fd4c687cde1
Sander Moolin <sandermoolin@gmail.com>
2021-06-15 10:02:52 -0400

    group as object pool working

--- src/plugin/components/groups/PhaserGroupChild.js
+++ src/plugin/components/groups/PhaserGroupChild.js
@@ -37,10 +37,8 @@ export default {
         }
     },
     beforeDestroy() {
-        if (this.group && this.group.killAndHide && this.target) {
-            // SOMETHING NOT WORKING HERE
-            const t = this.target
-            this.group.killAndHide.bind(this.group)(t)
+        if (this && this.group && this.group.killAndHide && this.target) {
+            this.group.killAndHide.bind(this.group)(this.target)
         }
 
         // if (this.target && this.target.ki) {
