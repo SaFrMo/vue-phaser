1ed3023bf85c84a9d12a727ba5b4ad6f8dbae829
Sander Moolin <sandermoolin@gmail.com>
2021-06-18 21:36:53 -0400

    Switching PhaserGroupChild to PhaserSprite

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,55 @@
 <template>
     <div id="app">
-        <phaser-game>
+        <phaser-game :options="config">
             <phaser-scene
                 scene-key="loading"
                 auto-start
                 :preload-queue="preloadQueue"
             >
-                <phaser-sprite sprite-key="space" x="400" y="300" />
+                <phaser-pool>
+                    <phaser-sprite
+                        pool-key="42"
+                        sprite-key="42"
+                        physics-name="42"
+                    />
+                </phaser-pool>
+
+                <phaser-sprite
+                    pool-key="42"
+                    :x="300 + 100 * (i - 1)"
+                    :y="300"
+                    scale="5"
+                    v-for="i in 3"
+                    :key="i"
+                />
+
+                <player-sprite />
             </phaser-scene>
         </phaser-game>
     </div>
 </template>
 
 <script lang="ts">
+import PlayerSprite from './PlayerSprite.vue'
+
 import Vue from 'vue'
 import VuePhaserPlugin from './plugin'
 Vue.use(VuePhaserPlugin)
 
 export default Vue.extend({
     name: 'App',
+    components: { PlayerSprite },
     data() {
         return {
-            preloadQueue: ['space.png'],
+            config: {
+                physics: {
+                    default: 'arcade',
+                    arcade: {
+                        debug: true,
+                    },
+                },
+            },
+            preloadQueue: ['space.png', '41.png', '42.png', '43.png'],
         }
     },
 })
--- src/plugin/components/core/PhaserSprite.js
+++ src/plugin/components/core/PhaserSprite.js
@@ -6,48 +6,66 @@ export default {
     name: 'phaser-sprite',
     mixins: [crop, customProps, depth, gameObject, origin, physics, transform],
     props: {
-        spriteKey: {
+        groupKey: {
             type: String,
+            default: null,
+        },
+        poolKey: {
+            type: String,
+            default: null,
         },
         spriteFrame: {
             type: [String, Number],
             default: null
-        }
-        // usePhysics: {
-        //     type: Boolean,
-        //     default: true
-        // }
+        },
+        spriteKey: {
+            type: String,
+        },
     },
     created() {
         let addToHost = true
-        let args = [this.x, this.y, this.spriteKey, this.spriteFrame]
+        const key = this.spriteKey || this.groupKey || this.poolKey
+        const args = [this.x, this.y, key, this.spriteFrame]
 
         if (this.isChildOfGroup) {
             addToHost = false
-            // get first available target
+            // add instance to group and hide
             this.target = this.$host.create(...args, false, false)
             this.$host.killAndHide.bind(this.$host)(this.target)
+        } else if (this.groupKey !== null || this.poolKey !== null) {
+            // get or create group instance
+            this.target = this.group.getFirstDead(false, ...args)
 
-            // if (!this.target) {
-            //     // if none ready, make a new one
-            //     this.target = this.$host.create(...args)
-            // }
-            // } else if (this.$host && this.$host.type === 'PhysicsGroup') {
-            //     addToHost = false
-            //     this.target = this.$host.create(...args)
-            //     this.$physics.world.emit('added', this.target)
+            // if none ready, make a new one
+            if (!this.target) {
+                this.target = this.group.create(...args)
+                this.$emit('created-new', this.target)
+            } else {
+                this.$emit('used-existing', this.target)
+            }
+
+            this.target.setActive(true)
+            this.target.setVisible(true)
+            this.refreshCustomProps()
+            this.$emit('activated', { item: this.target })
         } else if (this.usePhysics && this.$scene.physics) {
+            // create single physics instance
             this.target = this.$physics.add.sprite(...args)
             this.$physics.world.emit('added', this.target)
         } else {
+            // create standard sprite
             this.target = this.$scene.add.sprite(...args)
         }
 
+        // add to host if needed
         if (this.$host && addToHost) {
             this.$host.add(this.target)
         }
     },
     computed: {
+        group() {
+            return this.$groups[this.groupKey || this.poolKey]
+        },
         isChildOfGroup() {
             return this.$host && (this.$host.type === 'Group' || this.$host.type === 'PhysicsGroup')
         }
